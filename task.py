8b97a62d03de204f7043f7563fc22a21adb4c3ec
funnyuser97 <bogdan.stupka12@gmail.com>
2017-11-08 17:24:39 +0200

    second commit

--- task.py
+++ task.py
@@ -1,3 +1,21 @@
+#program, which sort list bubble aldorithm
+from random import randint
+#constant
+MAX_EL=99
+MIN_EL=0
+SIZE_MAS=100
+#main function
+def main():
+    #create and filling unsorted int list
+    list_int=[randint(MIN_EL,MAX_EL) for i in range(SIZE_MAS)]
+    print('int: ',list_int)
+
+    bubble_sort(SIZE_MAS,list_int)
+    print('\nbubble sort list is sorted correctly = true:\n',list_int)  
+
+    return;
+
+#Sort lists of elements using the buuble sort algorithm.
 def bubble_sort(max_size,list_bubble):
     for i in range(max_size):
         for j in range(max_size-1):
@@ -5,6 +23,4 @@ def bubble_sort(max_size,list_bubble):
                 list_bubble[j],list_bubble[j+1]=list_bubble[j+1],list_bubble[j]
     return;
 
-listq=[5,8,1,6,3,0]
-bubble_sort(6,listq)
-print(listq)
+main()
